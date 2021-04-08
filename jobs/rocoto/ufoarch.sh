2cca3c58cc11460ead30796a6aaaa44249e241d3
Cory Martin <cory.r.martin@noaa.gov>
2021-04-08 07:29:47 -0500

    Change archive directory convention

--- jobs/rocoto/ufoarch.sh
+++ jobs/rocoto/ufoarch.sh
@@ -11,14 +11,14 @@ fPDY=$(echo $FDATE | cut -c1-8)
 fcyc=$(echo $FDATE | cut -c9-10)
 
 # create ARCHDIR
-mkdir -p $ARCHDIR/$FDATE/$CDUMP.$PDY/$cyc/
+mkdir -p $ARCHDIR/$CDATE/$CDUMP.$PDY/$cyc/
 
 # move output from ROTDIR to ARCHDIR
-mv -f $ROTDIR/$CDUMP.$PDY/$cyc/* $ARCHDIR/$FDATE/$CDUMP.$PDY/$cyc/.
+mv -f $ROTDIR/$CDUMP.$PDY/$cyc/* $ARCHDIR/$CDATE/$CDUMP.$PDY/$cyc/.
 
 # link bias corr files
-mkdir -p $ARCHDIR/$FDATE/$CDUMP.$fPDY/$fcyc/atmos
-ln -sf $ICSDIR/$FDATE/$CDUMP.$PDY/$cyc/*abias* $ARCHDIR/$FDATE/$CDUMP.$PDY/$cyc/atmos/.
+mkdir -p $ARCHDIR/$CDATE/$CDUMP.$fPDY/$fcyc/atmos
+ln -sf $ICSDIR/$FDATE/$CDUMP.$PDY/$cyc/*abias* $ARCHDIR/$CDATE/$CDUMP.$PDY/$cyc/atmos/.
 
 # move the ROT to start fresh; can delete later if requested...
 mkdir -p $ROTDIR/$CDATE
