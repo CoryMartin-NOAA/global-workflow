7996a4b516ca7b0ad54ac35d275781dccbbc370e
Travis Elless <travis.j.elless@noaa.gov>
2026-06-11 19:33:15 +0000

    remove gfs part 2

--- dev/workflow/applications/gfs_cycled.py
+++ dev/workflow/applications/gfs_cycled.py
@@ -299,7 +299,7 @@ class GFSCycledAppConfig(AppConfig):
                     if options['do_gsisoilda']:
                         task_names[run] += ['sfcanl_regrid']
                     if options['do_jediocnvar'
-                        task_names[run] += ['marinebmatinit', 'marinebmat'
+                        task_names[run] += ['marinebmatinit', 'marinebmat']
                 else:
                     if options['do_wave']:
                         task_names[run] += wave_prep_tasks
